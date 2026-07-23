35ed142507b15c86442af9a278c358aa3ae21407
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2026-07-23 18:23:48 +0300

    new systems
    too lazy to promptly name my commits lol

--- scripts/character.gd
+++ scripts/character.gd
@@ -4,19 +4,16 @@ extends CharacterBody2D
 @export var input_component: InputComponent
 @export var movement_component: MovementComponent
 @export var camera_control_component: CameraControlComponent
+@export var animation_state_component: AnimationStateComponent
 
 @onready var visibles := $visibleItems
-
-var flip := true
 
 func _update_components(delta: float) -> void:
 	input_component.update()
 	movement_component.dir = input_component.movement_direction
 	movement_component.tick(delta)
 	camera_control_component.update()
+	animation_state_component.update()
 	
 func _physics_process(delta: float) -> void:
-	if input_component.movement_direction and (velocity.x > 0) != flip:
-		flip = velocity.x >= 0
-	visibles.scale.x = 1 if flip else -1
 	_update_components(delta)
