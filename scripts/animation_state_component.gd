ae378bff18d19759ab3804ce51610e68aee9fd08
BoikoMaximAlexeevych <147051218+BoikoMaximAlexeevych@users.noreply.github.com>
2026-07-26 11:25:06 +0300

    added smoking

--- scripts/animation_state_component.gd
+++ scripts/animation_state_component.gd
@@ -2,14 +2,21 @@ class_name AnimationStateComponent extends Node
 
 @export var movement_component: MovementComponent
 @export var animation_tree: AnimationTree
+@onready var input_component: InputComponent = %InputComponent
+
 
 var velocity := 0.
 var direction := Vector2.ZERO
 var last_dir := 0.
+var smoking := false
 
 func update():
 	velocity = movement_component.current_speed
 	direction = movement_component.dir
+	if input_component.smoke_pressed:
+		smoking = true
+		await get_tree().create_timer(5.).timeout
+		smoking = false
 	
 	if velocity != 0.:
 		last_dir = direction.x
--- scripts/input_component.gd
+++ scripts/input_component.gd
@@ -1,14 +1,19 @@
 class_name InputComponent
 extends Node
 
+@onready var animation_state_component: AnimationStateComponent = $"../AnimationStateComponent"
+
+
 var movement_direction: Vector2 = Vector2.ZERO
 var jump_pressed: bool = false
 var enter_pressed: bool = false
 var monologue_skip_pressed: bool = false
+var smoke_pressed: bool = false
 
 func update() -> void:
-	movement_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	jump_pressed = Input.is_action_just_pressed("move_jump")
-	enter_pressed = Input.is_action_just_pressed("interact")
-	monologue_skip_pressed = Input.is_action_just_pressed("monologue_skip")
-	
+	if not animation_state_component.smoking:
+		movement_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+		jump_pressed = Input.is_action_just_pressed("move_jump")
+		enter_pressed = Input.is_action_just_pressed("interact")
+		monologue_skip_pressed = Input.is_action_just_pressed("monologue_skip")
+	smoke_pressed = Input.is_action_just_pressed("smoke")
